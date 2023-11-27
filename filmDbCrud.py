1d7a2ddce65ea05bc3dd02a7c0800fae6150a6e4
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-11-27 10:23:59 +0000

    Add addFilm function to filmDbCrud.py

--- filmDbCrud.py
+++ filmDbCrud.py
@@ -3,3 +3,8 @@ from connect import *
 def readAll():
   dbCur.execute("SELECT * FROM tblFilms")
   return dbCur.fetchall()
+
+def addFilm(filmTitle, filmReleaseYear, filmRating, filmDuration, filmGenre):
+  dbCur.execute("INSERT INTO tblFilms(title, yearReleased, rating, duration, genre) VALUES (?, ?, ? ,? ,?)",
+              (filmTitle, filmReleaseYear, filmRating, filmDuration, filmGenre))
+  dbCon.commit()
