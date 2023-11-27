749c50e54f3f529b5cada6d9b2f6cd65856c7bf8
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-11-27 12:10:39 +0000

    Add getFilmsOfYear function to reports.py

--- reports.py
+++ reports.py
@@ -3,3 +3,7 @@ from connect import *
 def getFilmsOfGenre(genre):
   dbCur.execute("SELECT * FROM tblFilms WHERE genre = ?", (genre,))
   return dbCur.fetchall()
+
+def getFilmsOfYear(releaseYear):
+  dbCur.execute("SELECT * FROM tblFilms WHERE yearReleased = ?", (releaseYear,))
+  return dbCur.fetchall()
