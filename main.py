3906eacf78f35165b8d714998caa4f6140d0b743
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-11-27 13:04:22 +0000

    Refactor functions for better encapsulation

--- main.py
+++ main.py
@@ -1,23 +1,14 @@
 import CRUD, reports
 
-mainMenuOptions = {
-  1: "Add a film",
-  2: "Delete a film",
-  3: "Update a film",
-  4: "Show all films",
-  5: "View film reports",
-  6: "Exit"
-}
-
-def getMainMenuText():
+def getMainMenuText(mainMenuOptions):
   text = "\nMAIN MENU\n"
   for key, value in mainMenuOptions.items():
     text = text + str(key) + ". " + value + "\n"
   return text
 
-def getMainMenuInput():
+def getMainMenuInput(mainMenuOptions):
   while True:
-      print(getMainMenuText())
+      print(getMainMenuText(mainMenuOptions))
       try:
         choice = int(input("What would you like to do? "))
         if (choice in mainMenuOptions.keys()):
@@ -28,12 +19,20 @@ def getMainMenuInput():
         print("\nIncorrect input, enter the number correlating to your choice")
 
 def mainProgram():
+  mainMenuOptions = {
+  1: "Add a film",
+  2: "Delete a film",
+  3: "Update a film",
+  4: "Show all films",
+  5: "View film reports",
+  6: "Exit"
+  }
+  
   programRunning = True
 
   while programRunning:
     print("Welcome to FilmFlix!")
-    choice = getMainMenuInput()
-    print(choice)
+    choice = getMainMenuInput(mainMenuOptions)
     programRunning = False
 
 
