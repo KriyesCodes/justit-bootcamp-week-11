ed2730ed0199f1193eca444657f9d7a8610fe359
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-11-27 11:20:22 +0000

    Add updateFilm function to filmDbCrud.py

--- filmDbCrud.py
+++ filmDbCrud.py
@@ -8,3 +8,21 @@ def addFilm(filmTitle, filmReleaseYear, filmRating, filmDuration, filmGenre):
   dbCur.execute("INSERT INTO tblFilms(title, yearReleased, rating, duration, genre) VALUES (?, ?, ? ,? ,?)",
               (filmTitle, filmReleaseYear, filmRating, filmDuration, filmGenre))
   dbCon.commit()
+
+def updateFilm(filmId, fieldName, fieldValue):
+  fieldName = fieldName.lower()
+  if (fieldName == 'title'):
+    query = "UPDATE tblFilms SET title = ? WHERE filmID = ?"
+  elif (fieldName == 'yearreleased'):
+    query = "UPDATE tblFilms SET yearReleased = ? WHERE filmID = ?"
+  elif (fieldName == 'rating'):
+    query = "UPDATE tblFilms SET rating = ? WHERE filmID = ?"
+  elif (fieldName == 'duration'):
+    query = "UPDATE tblFilms SET duration = ? WHERE filmID = ?"
+  elif (fieldName == 'genre'):
+    query = "UPDATE tblFilms SET genre = ? WHERE filmID = ?"
+  else:
+    raise Exception("Field name given does not match any in the database table")
+  
+  dbCur.execute(query, (fieldValue, filmId))
+  dbCon.commit()
