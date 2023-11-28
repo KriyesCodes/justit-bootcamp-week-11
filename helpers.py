23cbd769a7e36b9ee7225b94556c5519188f7aab
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2023-11-28 20:38:36 +0000

    Reformt helpers.py functions

--- helpers.py
+++ helpers.py
@@ -18,31 +18,51 @@ def getMenuInput(menuOptions):
       except:
         print("\nIncorrect input, enter the number correlating to your choice")
 
-# This takes a film record tuple and returns a string with all the data in the tuple in a table row format
-def getTableRowFormattedFilmRecord(filmRecord):
-  maxStringLength = 15
-  colWidth = 20
+def formatColumnValues(title, year, rating, duration, genre, maxStringLength):
+  title = str(title)
+  year = str(year)
+  rating = str(rating)
+  duration = str(duration)
+  genre = str(genre)
+
+  if len(title) > maxStringLength:
+    title = title[0:maxStringLength]+"..."
+  if len(year) > maxStringLength:
+    year = year[0:maxStringLength]+"..."
+  if len(rating) > maxStringLength:
+    rating = rating[0:maxStringLength]+"..."
+  if len(duration) > maxStringLength:
+    duration = duration[0:maxStringLength]+"..."
+  if len(genre) > maxStringLength:
+    genre = genre[0:maxStringLength]+"..."
+
+  return (title, year, rating, duration, genre)
+
+# Takes a tuple containing the field names of the table, and a max string length and column width to format it to
+# returns a string formatted to look like a table heading
+def getTableHeadingFormatted(filmHeadings, maxStringLength, colWidth):
+  if len(filmHeadings) != 6:
+    raise Exception("Tuple length must be 6")
+  else:
+    title, year, rating, duration, genre = formatColumnValues(filmHeadings[1], filmHeadings[2], filmHeadings[3], filmHeadings[4], filmHeadings[5], maxStringLength)
+
+  formattedString = "";
+
+  for i in range(colWidth * len(filmHeadings)-1):
+    formattedString += "_"
+  formattedString += "\n"
+  formattedString += "|{:^{max}}|{:^{max}}|{:^{max}}|{:^{max}}|{:^{max}}|".format(title, year, rating, duration, genre, max=colWidth)
+
+  return formattedString
+# Takes a film record tuple and a max string length and column width to format it to
+#  returns a string formatted to look like a table row
+def getTableRowFormattedFilmRecord(filmRecord, maxStringLength, colWidth):
   if len(filmRecord) != 6:
     raise Exception("Tuple length must be 6")
   else:
-    title = str(filmRecord[1])
-    year = str(filmRecord[2])
-    rating = str(filmRecord[3])
-    duration = str(filmRecord[4])
-    genre = str(filmRecord[5])
-
-    if len(title) > maxStringLength:
-      title = title[0:maxStringLength]+"..."
-    if len(year) > maxStringLength:
-      year = year[0:maxStringLength]+"..."
-    if len(rating) > maxStringLength:
-      rating = rating[0:maxStringLength]+"..."
-    if len(duration) > maxStringLength:
-      duration = duration[0:maxStringLength]+"..."
-    if len(genre) > maxStringLength:
-      genre = genre[0:maxStringLength]+"..."
+    title, year, rating, duration, genre = formatColumnValues(filmRecord[1], filmRecord[2], filmRecord[3], filmRecord[4], filmRecord[5], maxStringLength)
 
   return("|{:^{max}}|{:^{max}}|{:^{max}}|{:^{max}}|{:^{max}}|".format(title, year, rating, duration, genre, max=colWidth))
 
 if __name__ == "__main__":
-  print(getTableRowFormattedFilmRecord(db.readFilm(16)[0]))
+  print(getTableRowFormattedFilmRecord(db.readFilm(16)[0]), 15, 20)
